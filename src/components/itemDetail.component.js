575c453f9d5a994b0cfcdd13d96c02d23af96090
moserhu <moserhu@gmail.com>
2023-01-29 12:09:33 -0600

    initial commit

---
+++ src/components/itemDetail.component.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import '../App.css';
+import { useParams } from 'react-router-dom';
+import axios from "axios";
+const API_URL = "http://localhost:8080/";
+
+
+
+function ItemDetail() {
+
+    const [item, setItem] = useState([]);
+    const id = useParams();
+    const [desc, setDesc] = useState([]);
+
+    useEffect(() => {
+        fetchItem()
+    }, []);
+
+
+    const fetchItem = async () => {
+
+            const fetchItem = await fetch(`https://www.dnd5eapi.co/api/${id.url}/${id.id}`);
+    
+            if (fetchItem.status === 404) {
+                const data = await axios.get(API_URL + `api/${id.url}`);
+                const items = await data.data;
+                const item = (items.filter(x => x.index === `${id.id}`)[0])
+                setItem(item);
+                setDesc(item.desc);
+            } else {
+                const item = await fetchItem.json();
+                setItem(item);
+                setDesc(item.desc);
+            }
+
+        }
+
+        return (
+            <div>
+                <h1 className='title'>
+                    {item.name}
+                </h1>
+                <div className='text'>
+                    <div className='itemType'>
+                        {item.desc}
+                        {desc.length === 0 &&
+                            <div>
+                                No description available, ask your DM for more inforomation.
+                            </div>
+                        }
+                    </div>
+                </div>
+            </div>
+        );
+
+};
+
+
+
+
+export default ItemDetail;
---
+++ src/services/auth.service.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/auth/";
+
+class AuthService {
+  login(username, password) {
+    return axios
+      .post(API_URL + "signin", {
+        username,
+        password
+      })
+      .then(response => {
+        if (response.data.accessToken) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+        
+        return response.data;
+      });
+  }
+
+  logout() {
+    localStorage.removeItem("user");
+    localStorage.removeItem("ShopItems");
+    localStorage.removeItem("TavernItems");
+  }
+
+  register(username, firstName, lastName, email, password) {
+    return axios.post(API_URL + "signup", {
+      username,
+      firstName,
+      lastName,
+      email,
+      password
+    });
+  }
+
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem('user'));;
+  }
+}
+
+export default new AuthService();
