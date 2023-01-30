7d4c8f5a0c82bb38caf385cbac8545221901f3c6
moserhu <moserhu@gmail.com>
2023-01-30 13:47:33 -0600

    might have fixed it lol

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ require("dotenv").config();
 
 const app = express();
 
+/*
 var corsOptions = {
   origin: "http://localhost:3000"
-};
+}; */
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(express.json());
